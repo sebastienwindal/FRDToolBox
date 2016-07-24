37ca68c036afba6489ca40de06d2d2f87b5d6bb2
Sebastien Windal <sebastien@windal.net>
2016-07-24 15:40:25 -0700

    Initial Commit

---
+++ FRDToolBox/FRDToolBox.h
@@ -0,0 +1,19 @@
+//
+//  FRDToolBox.h
+//  FRDToolBox
+//
+//  Created by Sebastien Windal on 7/24/16.
+//  Copyright © 2016 Sebastien Windal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FRDToolBox.
+FOUNDATION_EXPORT double FRDToolBoxVersionNumber;
+
+//! Project version string for FRDToolBox.
+FOUNDATION_EXPORT const unsigned char FRDToolBoxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FRDToolBox/PublicHeader.h>
+
+
